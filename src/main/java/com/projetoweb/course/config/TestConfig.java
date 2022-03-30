9388da83c50af40ea7cb47415db9218f376bc2f2
Gabriel Rocha <fernandeswb7@hotmail.com>
2022-03-30 16:04:34 -0300

    Refactor and Update

--- src/main/java/com/projetoweb/course/config/TestConfig.java
+++ src/main/java/com/projetoweb/course/config/TestConfig.java
@@ -1,10 +1,7 @@
 package com.projetoweb.course.config;
 
-import com.projetoweb.course.entities.Order;
-import com.projetoweb.course.entities.User;
-import com.projetoweb.course.entities.enums.OrderStatus;
-import com.projetoweb.course.repositories.OrderRepository;
-import com.projetoweb.course.repositories.UserRepository;
+import com.projetoweb.course.entities.*;
+import com.projetoweb.course.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -17,33 +14,63 @@ import java.util.Arrays;
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
 
-    @Autowired
-    private UserRepository repository;
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
-    @Autowired
-    private OrderRepository orderRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), com.educandoweb.course.entities.enums.OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), com.educandoweb.course.entities.enums.OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), com.educandoweb.course.entities.enums.OrderStatus.WAITING_PAYMENT, u1);
+		
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
-    @Override
-    public void run(String... args) {
-        User user = User.builder()
-                .email("maria@email.com")
-                .name("Maria")
-                .phone("33999994444")
-                .password("1234")
-                .build();
-
-        User userTwo = User.builder()
-                .email("joao@email.com")
-                .name("Joao")
-                .phone("33888881111")
-                .password("1234")
-                .build();
-
-        Order orderOne = Order.builder().moment(Instant.parse("2019-06-20T19:53:07Z")).orderStatus(OrderStatus.DELIVERED).client(user).build();
-        Order orderTwo = Order.builder().moment(Instant.parse("2022-02-20T18:33:44Z")).orderStatus(OrderStatus.CANCELED).client(userTwo).build();
-        Order orderThree = Order.builder().moment(Instant.parse("2021-04-20T20:54:27Z")).orderStatus(OrderStatus.WAITING_PAYMENT).client(user).build();
-
-        repository.saveAll(Arrays.asList(user, userTwo));
-        orderRepository.saveAll(Arrays.asList(orderOne, orderTwo, orderThree));
-    }
+		orderRepository.save(o1);
+	}
 }
--- src/main/java/com/projetoweb/course/entities/Product.java
+++ src/main/java/com/projetoweb/course/entities/Product.java
@@ -1,52 +1,129 @@
 package com.projetoweb.course.entities;
 
-import lombok.*;
-
-import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Builder
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "tb_product")
 public class Product implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-    private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
 
-    @Column
-    private String name;
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+	
+	public Product() {
+	}
 
-    @Column
-    private String description;
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+	}
 
-    @Column
-    private Double price;
+	public Long getId() {
+		return id;
+	}
 
-    @Column
-    private String imgUrl;
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    @ManyToMany
-    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	public String getDescription() {
+		return description;
+	}
 
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
+	public Double getPrice() {
+		return price;
+	}
 
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 
+	public String getImgUrl() {
+		return imgUrl;
+	}
 
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
 
+	public Set<Category> getCategories() {
+		return categories;
+	}
 
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		Set<Order> orders = items.stream().map(orderItem -> {
+			return orderItem.getOrder();
+		}).collect(Collectors.toSet());
 
+		return orders;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 
-
-
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
