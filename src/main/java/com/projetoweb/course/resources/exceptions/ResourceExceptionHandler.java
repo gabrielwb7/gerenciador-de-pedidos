211e042330fd94b692d05d2a95141ba5ec501ed0
Gabriel Rocha <fernandeswb7@hotmail.com>
2022-03-31 11:30:34 -0300

    Exception handling - delete

--- src/main/java/com/projetoweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/projetoweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,6 +1,8 @@
-package com.projetoweb.course.services.exceptions;
+package com.projetoweb.course.resources.exceptions;
 
 import com.projetoweb.course.resources.exceptions.StandardError;
+import com.projetoweb.course.services.exceptions.DataBaseException;
+import com.projetoweb.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,6 +18,15 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException exception, HttpServletRequest request) {
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, exception.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> dataBaseException(DataBaseException exception, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, exception.getMessage(), request.getRequestURI());
 
         return ResponseEntity.status(status).body(err);
--- src/main/java/com/projetoweb/course/services/UserService.java
+++ src/main/java/com/projetoweb/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.projetoweb.course.services;
 
 import com.projetoweb.course.entities.User;
 import com.projetoweb.course.repositories.UserRepository;
+import com.projetoweb.course.services.exceptions.DataBaseException;
 import com.projetoweb.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +31,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataBaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User newData) {
